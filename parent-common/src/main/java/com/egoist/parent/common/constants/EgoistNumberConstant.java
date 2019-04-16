5f576ee8042cc1e0d41094d9d2c47644b05d2321
JingshengYe <jingsheng-ye@msyc.cc>
2019-04-16 21:03:56 +0800

    :sparkles: 新增数字常量

---
+++ parent-common/src/main/java/com/egoist/parent/common/constants/EgoistNumberConstant.java
@@ -0,0 +1,49 @@
+package com.egoist.parent.common.constants;
+
+public final class EgoistNumberConstant {
+    public static final int ONE = 1;
+
+    public static final int TWO = 2;
+
+    /**
+     * 3
+     */
+    public static final int THREE = 3;
+
+    /**
+     * 4
+     */
+    public static final int FOUR = 4;
+
+    /**
+     * 5
+     */
+    public static final int FIVE = 5;
+
+    /**
+     * 6
+     */
+    public static final int SIX = 6;
+
+    /**
+     * 7
+     */
+    public static final int SEVEN = 7;
+
+    /**
+     * 8
+     */
+    public static final int EIGHT = 8;
+
+    /**
+     * 9
+     */
+    public static final int NINE = 9;
+
+    /**
+     * 10
+     */
+    public static final int TEN = 10;
+
+    public static final int ONE_HUNDRED = 100;
+}
